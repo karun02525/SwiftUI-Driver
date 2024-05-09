c914a0f54e97b2aedea7a76bb6960ed3171514e3
karunkumar <karun.kumar@hidglobal.com>
2024-05-09 18:09:22 +0530

    Initial Commit

---
+++ DriverApp/DriverAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverAppApp.swift
+//  DriverApp
+//
+//  Created by Karun Kumar on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DriverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
